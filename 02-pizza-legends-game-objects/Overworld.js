3519c5506135d8b19ec80b2b0f7271e9d47da3c8
Ramon Gonzalez Casillas <56400522+Mosquiscript@users.noreply.github.com>
2023-04-11 18:14:05 -0600

    Develop pizza game

---
+++ 02-pizza-legends-game-objects/Overworld.js
@@ -0,0 +1,46 @@
+class Overworld {
+ constructor(config) {
+   this.element = config.element;
+   this.canvas = this.element.querySelector(".game-canvas");
+   this.ctx = this.canvas.getContext("2d");
+   this.map = null;
+ }
+
+     startGameLoop()
+     {
+
+         const step = () =>
+         {
+              this.ctx.clearRect(0,0, this.canvas.width, this.canvas.height);
+            
+             //Draw lower Layer
+             this.map.drawLowerImage(this.ctx);
+
+             //Draw Objects
+             Object.values(this.map.gameObjects).forEach(object =>{
+                 object.y += 1;
+                 object.sprite.draw(this.ctx);
+
+             })
+
+             //Draw Upper Layer
+             this.map.drawUpperImage(this.ctx);
+            
+             requestAnimationFrame(() => {
+                 console.log("stepping!");
+                 step();
+             })
+         }
+         step();
+     }
+
+     init()
+     {
+            this.map = new OverworldMap(window.OverworldMaps.Kitchen);
+            this.startGameLoop();
+          
+
+
+     }
+
+}
